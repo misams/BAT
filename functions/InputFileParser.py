b50dbe52069610a55e129d32228c8f18763ff7c0
misams <sams.m@gmx.net>
2020-04-05 11:18:26 +0200

    begin of error handling - first try

--- functions/InputFileParser.py
+++ functions/InputFileParser.py
@@ -46,109 +46,115 @@ class InputFileParser:
         print(log_str)
         logging.info(log_str)
         # process data
-        with open(self.input_file) as fid:
-            line = fid.readline() # first line in file
-            while line: # loop through input-file
-                if line.lstrip()[0:13]=="*PROJECT_NAME":
-                    self.project_name = self._proc_line(line)[1]
-                elif line.lstrip()[0:7]=="*METHOD":
-                    self.method = self._proc_line(line)[1]
-                elif line.lstrip()[0:16]=="*BOLT_DEFINITION":
-                    line = fid.readline()
-                    # loop through bolt-definition block
-                    while line.lstrip()[0:20]!="*BOLT_DEFINITION_END":
-                        tmp_line = self._proc_line(line) # process inp-file line
-                        if tmp_line[0]=="*JOINT_MOS_TYPE":
-                            self.joint_mos_type = tmp_line[1]
-                        elif tmp_line[0]=="*BOLT_SIZE":
-                            self.bolt_size = tmp_line[1]
-                        elif tmp_line[0]=="*BOLT_MATERIAL":
-                            self.bolt_material = tmp_line[1]
-                        elif tmp_line[0]=="*COF_CLAMP":
-                            self.cof_clamp = float(tmp_line[1])
-                        elif tmp_line[0]=="*COF_BOLT":
-                            # get (mu_head_max, mu_thread_max, mu_head_min, mu_thread_min)
-                            tmp_cof_bolt_str = tmp_line[1].replace('[','').replace(']','').split(',')
-                            if len(tmp_cof_bolt_str)==4:
-                                self.cof_bolt = (float(tmp_cof_bolt_str[0]), float(tmp_cof_bolt_str[1]), \
-                                    float(tmp_cof_bolt_str[2]), float(tmp_cof_bolt_str[3]))
-                            else:
-                                #TODO: error handling
-                                logging.error("ERROR: 4 input parameters (COF_BOLT) neccessary!",\
-                                    exc_info=True)
-                        elif tmp_line[0]=="*TIGHT_TORQUE":
-                            self.tight_torque = float(tmp_line[1])
-                        elif tmp_line[0]=="*BOLT_HEAD_TYPE":
-                            self.bolt_head_type = tmp_line[1]
-                        elif tmp_line[0]=="*TORQUE_TOL_TIGHT_DEVICE":
-                            self.torque_tol_tight_device = float(tmp_line[1])
-                        elif tmp_line[0]=="*LOCKING_MECHANISM":
-                            self.locking_mechanism = tmp_line[1]
-                        elif tmp_line[0]=="*PREVAILING_TORQUE":
-                            self.prevailing_torque = float(tmp_line[1])
-                        elif tmp_line[0]=="*LOADING_PLANE_FACTOR":
-                            self.loading_plane_factor = float(tmp_line[1])
+        try:
+            with open(self.input_file) as fid:
+                line = fid.readline() # first line in file
+                while line: # loop through input-file
+                    if line.lstrip()[0:13]=="*PROJECT_NAME":
+                        self.project_name = self._proc_line(line)[1]
+                    elif line.lstrip()[0:7]=="*METHOD":
+                        self.method = self._proc_line(line)[1]
+                    elif line.lstrip()[0:16]=="*BOLT_DEFINITION":
                         line = fid.readline()
-                elif line.lstrip()[0:25]=="*CLAMPED_PARTS_DEFINITION":
-                    line = fid.readline()
-                    # loop through clamped-parts-definition block
-                    while line.lstrip()[0:29]!="*CLAMPED_PARTS_DEFINITION_END":
-                        tmp_line = self._proc_line(line) # process inp-file line
-                        if tmp_line[0]=="*NMBR_SHEAR_PLANES":
-                            self.nmbr_shear_planes = int(tmp_line[1])
-                        elif tmp_line[0]=="*USE_SHIM":
-                            if tmp_line[1] == "no":
-                                self.use_shim = "no"
-                            else:
-                                # get (shim-material, shim-type) or no
-                                tmp_shim_str = tmp_line[1].replace('[','').replace(']','').split(',')
-                                self.use_shim = (tmp_shim_str[0].lstrip().rstrip(), \
-                                    tmp_shim_str[1].lstrip().rstrip())
-                        elif tmp_line[0]=="*THROUGH_HOLE_DIAMETER":
-                            self.through_hole_diameter = float(tmp_line[1])
-                        elif tmp_line[0]=="*SUBST_DA":
-                            self.subst_da = tmp_line[1]
-                        elif tmp_line[0][:-3]=="*CLAMPED_PART":
-                            # get clamped-part number (inside brackets) and save n-CP to dict
-                            cp_nmbr = int(tmp_line[0][tmp_line[0].find("(")+1:tmp_line[0].find(")")])
-                            # get (clamped_part_material, thickness)
-                            tmp_cp_str = tmp_line[1].replace('[','').replace(']','').split(',')
-                            self.clamped_parts.update({cp_nmbr : (tmp_cp_str[0].strip(), float(tmp_cp_str[1]))})
+                        # loop through bolt-definition block
+                        while line.lstrip()[0:20]!="*BOLT_DEFINITION_END":
+                            tmp_line = self._proc_line(line) # process inp-file line
+                            if tmp_line[0]=="*JOINT_MOS_TYPE":
+                                self.joint_mos_type = tmp_line[1]
+                            elif tmp_line[0]=="*BOLT_SIZE":
+                                self.bolt_size = tmp_line[1]
+                            elif tmp_line[0]=="*BOLT_MATERIAL":
+                                self.bolt_material = tmp_line[1]
+                            elif tmp_line[0]=="*COF_CLAMP":
+                                self.cof_clamp = float(tmp_line[1])
+                            elif tmp_line[0]=="*COF_BOLT":
+                                # get (mu_head_max, mu_thread_max, mu_head_min, mu_thread_min)
+                                tmp_cof_bolt_str = tmp_line[1].replace('[','').replace(']','').split(',')
+                                if len(tmp_cof_bolt_str)==4:
+                                    self.cof_bolt = (float(tmp_cof_bolt_str[0]), float(tmp_cof_bolt_str[1]), \
+                                        float(tmp_cof_bolt_str[2]), float(tmp_cof_bolt_str[3]))
+                                else:
+                                    # raise error
+                                    raise ValueError("ERROR: 4 input parameters (COF_BOLT) neccessary! "+\
+                                        "Check syntax in input (*.inp) file.")
+                            elif tmp_line[0]=="*TIGHT_TORQUE":
+                                self.tight_torque = float(tmp_line[1])
+                            elif tmp_line[0]=="*BOLT_HEAD_TYPE":
+                                self.bolt_head_type = tmp_line[1]
+                            elif tmp_line[0]=="*TORQUE_TOL_TIGHT_DEVICE":
+                                self.torque_tol_tight_device = float(tmp_line[1])
+                            elif tmp_line[0]=="*LOCKING_MECHANISM":
+                                self.locking_mechanism = tmp_line[1]
+                            elif tmp_line[0]=="*PREVAILING_TORQUE":
+                                self.prevailing_torque = float(tmp_line[1])
+                            elif tmp_line[0]=="*LOADING_PLANE_FACTOR":
+                                self.loading_plane_factor = float(tmp_line[1])
+                            line = fid.readline()
+                    elif line.lstrip()[0:25]=="*CLAMPED_PARTS_DEFINITION":
                         line = fid.readline()
-                elif line.lstrip()[0:15]=="*FOS_DEFINITION":
-                    line = fid.readline()
-                    # loop through FOS-definition block
-                    while line.lstrip()[0:19]!="*FOS_DEFINITION_END":
-                        tmp_line = self._proc_line(line) # process inp-file line
-                        if tmp_line[0]=="*FOS_Y":
-                            self.fos_y = float(tmp_line[1])
-                        elif tmp_line[0]=="*FOS_U":
-                            self.fos_u = float(tmp_line[1])
-                        elif tmp_line[0]=="*FOS_SLIP":
-                            self.fos_slip = float(tmp_line[1])
-                        elif tmp_line[0]=="*FOS_GAP":
-                            self.fos_gap = float(tmp_line[1])
+                        # loop through clamped-parts-definition block
+                        while line.lstrip()[0:29]!="*CLAMPED_PARTS_DEFINITION_END":
+                            tmp_line = self._proc_line(line) # process inp-file line
+                            if tmp_line[0]=="*NMBR_SHEAR_PLANES":
+                                self.nmbr_shear_planes = int(tmp_line[1])
+                            elif tmp_line[0]=="*USE_SHIM":
+                                if tmp_line[1] == "no":
+                                    self.use_shim = "no"
+                                else:
+                                    # get (shim-material, shim-type) or no
+                                    tmp_shim_str = tmp_line[1].replace('[','').replace(']','').split(',')
+                                    self.use_shim = (tmp_shim_str[0].lstrip().rstrip(), \
+                                        tmp_shim_str[1].lstrip().rstrip())
+                            elif tmp_line[0]=="*THROUGH_HOLE_DIAMETER":
+                                self.through_hole_diameter = float(tmp_line[1])
+                            elif tmp_line[0]=="*SUBST_DA":
+                                self.subst_da = tmp_line[1]
+                            elif tmp_line[0][:-3]=="*CLAMPED_PART":
+                                # get clamped-part number (inside brackets) and save n-CP to dict
+                                cp_nmbr = int(tmp_line[0][tmp_line[0].find("(")+1:tmp_line[0].find(")")])
+                                # get (clamped_part_material, thickness)
+                                tmp_cp_str = tmp_line[1].replace('[','').replace(']','').split(',')
+                                self.clamped_parts.update({cp_nmbr : (tmp_cp_str[0].strip(), float(tmp_cp_str[1]))})
+                            line = fid.readline()
+                    elif line.lstrip()[0:15]=="*FOS_DEFINITION":
                         line = fid.readline()
-                elif line.lstrip()[0:21]=="*BOLT_LOAD_DEFINITION":
-                    line = fid.readline()
-                    # loop through bolt-load-definition block
-                    while line.lstrip()[0:25]!="*BOLT_LOAD_DEFINITION_END":
-                        # load-ID, axial-force, lateral-force-1, lateral-force-2 (optional)
-                        tmp_line = line.split()
-                        if len(tmp_line)==4: # optional lat-force-2 used
-                            self.bolt_loads.update({tmp_line[0] : (float(tmp_line[1]), float(tmp_line[2]), float(tmp_line[3]))})
-                        elif len(tmp_line)==3: # optional lat-force-2 NOT used
-                            self.bolt_loads.update({tmp_line[0] : (float(tmp_line[1]), float(tmp_line[2]), 0.0)})
+                        # loop through FOS-definition block
+                        while line.lstrip()[0:19]!="*FOS_DEFINITION_END":
+                            tmp_line = self._proc_line(line) # process inp-file line
+                            if tmp_line[0]=="*FOS_Y":
+                                self.fos_y = float(tmp_line[1])
+                            elif tmp_line[0]=="*FOS_U":
+                                self.fos_u = float(tmp_line[1])
+                            elif tmp_line[0]=="*FOS_SLIP":
+                                self.fos_slip = float(tmp_line[1])
+                            elif tmp_line[0]=="*FOS_GAP":
+                                self.fos_gap = float(tmp_line[1])
+                            line = fid.readline()
+                    elif line.lstrip()[0:21]=="*BOLT_LOAD_DEFINITION":
                         line = fid.readline()
-                elif line.lstrip()[0:16]=="*TEMP_DEFINITION":
-                    line = fid.readline()
-                    # loop through bolt-load-definition block
-                    while line.lstrip()[0:20]!="*TEMP_DEFINITION_END":
-                        tmp_line = self._proc_line(line) # process inp-file line
-                        if tmp_line[0]=="*DELTA_T":
-                            self.delta_t = float(tmp_line[1])
+                        # loop through bolt-load-definition block
+                        while line.lstrip()[0:25]!="*BOLT_LOAD_DEFINITION_END":
+                            # load-ID, axial-force, lateral-force-1, lateral-force-2 (optional)
+                            tmp_line = line.split()
+                            if len(tmp_line)==4: # optional lat-force-2 used
+                                self.bolt_loads.update({tmp_line[0] : (float(tmp_line[1]), float(tmp_line[2]), float(tmp_line[3]))})
+                            elif len(tmp_line)==3: # optional lat-force-2 NOT used
+                                self.bolt_loads.update({tmp_line[0] : (float(tmp_line[1]), float(tmp_line[2]), 0.0)})
+                            line = fid.readline()
+                    elif line.lstrip()[0:16]=="*TEMP_DEFINITION":
                         line = fid.readline()
-                line = fid.readline()
+                        # loop through bolt-load-definition block
+                        while line.lstrip()[0:20]!="*TEMP_DEFINITION_END":
+                            tmp_line = self._proc_line(line) # process inp-file line
+                            if tmp_line[0]=="*DELTA_T":
+                                self.delta_t = float(tmp_line[1])
+                            line = fid.readline()
+                    line = fid.readline()
+        # catch exceptions
+        except FileNotFoundError as fnf_error:
+            logging.error(fnf_error, exc_info=True)
+        except ValueError as val_error:
+            logging.error(val_error, exc_info=True)
 
     # process commented input file line
     def _proc_line(self, line):
