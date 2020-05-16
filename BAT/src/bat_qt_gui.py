0c27fabc6e70eb71bc4674acfe43ed8bb4f7289c
misams <sams.m@gmx.net>
2020-05-16 22:55:24 +0200

    pyQt gui work 5

--- BAT/src/bat_qt_gui.py
+++ BAT/src/bat_qt_gui.py
@@ -2,9 +2,11 @@
 import sys
 import os
 from pathlib import Path
+from collections import Counter, OrderedDict
 from src.functions.InputFileParser import InputFileParser
 from PyQt5 import QtWidgets, uic, QtCore
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.Qt import Qt, QApplication, QClipboard
 
 # inherit correct QMainWindow class as defined in UI file (designer)
 class Ui(QtWidgets.QMainWindow):
@@ -32,17 +34,13 @@ class Ui(QtWidgets.QMainWindow):
         self.actionQuit.triggered.connect(self.close)
         # base options
         self.radioEsaPss = self.findChild(QtWidgets.QRadioButton, "radioEsaPss")
-        self.radioEsaPss.setChecked(True)
         self.radioEcss = self.findChild(QtWidgets.QRadioButton, "radioEcss")
-        self.radioEcss.setEnabled(False) # not implemented yet
         self.radioVdi = self.findChild(QtWidgets.QRadioButton, "radioVdi")
-        self.radioVdi.setEnabled(False) # not implemented yet
         self.projectName = self.findChild(QtWidgets.QLineEdit, "projectName")
+        self.tabWidget = self.findChild(QtWidgets.QTabWidget, "tabWidget")
         # Bolt tab
         self.radioJointMin = self.findChild(QtWidgets.QRadioButton, "radioJointMin")
-        self.radioJointMin.setChecked(True)
         self.radioJointMean = self.findChild(QtWidgets.QRadioButton, "radioJointMean")
-        self.radioJointMean.setEnabled(False) # not implemented yet
         self.comboBolt = self.findChild(QtWidgets.QComboBox, "comboBolt")
         self.comboBoltMaterial = self.findChild(QtWidgets.QComboBox, "comboBoltMaterial")
         self.cofBoltHeadMin = self.findChild(QtWidgets.QLineEdit, "cofBoltHeadMin")
@@ -53,7 +51,6 @@ class Ui(QtWidgets.QMainWindow):
         self.tightTorqueTol = self.findChild(QtWidgets.QLineEdit, "tightTorqueTol")
         self.radioLockYes = self.findChild(QtWidgets.QRadioButton, "radioLockYes")
         self.radioLockYes.toggled.connect(self.lockRadioClicked)
-        self.radioLockYes.setChecked(True)
         self.radioLockNo = self.findChild(QtWidgets.QRadioButton, "radioLockNo")
         self.radioLockNo.toggled.connect(self.lockRadioClicked)
         self.prevailingTorque = self.findChild(QtWidgets.QLineEdit, "prevailingTorque")
@@ -61,7 +58,6 @@ class Ui(QtWidgets.QMainWindow):
         # Clamped Parts tab
         self.cofClampedParts = self.findChild(QtWidgets.QLineEdit, "cofClampedParts")
         self.numberOfShearPlanes = self.findChild(QtWidgets.QSpinBox, "numberOfShearPlanes")
-        self.numberOfShearPlanes.setValue(1)
         self.throughHoleDiameter = self.findChild(QtWidgets.QLineEdit, "throughHoleDiameter")
         self.substDiameter = self.findChild(QtWidgets.QLineEdit, "substDiameter")
         self.clampedPartsTable = self.findChild(QtWidgets.QTableWidget, "clampedPartsTable")
@@ -77,15 +73,12 @@ class Ui(QtWidgets.QMainWindow):
         self.fosGap= self.findChild(QtWidgets.QLineEdit, "fosGap")
         # Loads tab
         self.loadsTable = self.findChild(QtWidgets.QTableWidget, "loadsTable")
-        self.loadsTable.setColumnCount(4) # init load-table
-        self.loadsTable.insertRow(0)
-        self.loadsTable.setHorizontalHeaderLabels(\
-            ["Bolt-ID\n\nLoad-Case", "FN\n\n[N]", "FQ1\n\n[N]", "FQ2\n(optional)\n[N]"])
-        self.loadsTable.setItem(0,0,QtWidgets.QTableWidgetItem("test bolt"))
         self.addRowButton = self.findChild(QtWidgets.QPushButton, "addRowButton")
         self.addRowButton.clicked.connect(self.addRowPressed)
         self.deleteRowButton = self.findChild(QtWidgets.QPushButton, "deleteRowButton")
         self.deleteRowButton.clicked.connect(self.deleteRowPressed)
+        self.pasteLoadsExcel = self.findChild(QtWidgets.QPushButton, "pasteLoadsExcel")
+        self.pasteLoadsExcel.clicked.connect(self.pasteFromExcel)
         self.deltaT= self.findChild(QtWidgets.QLineEdit, "deltaT")
         # Calculate tab
         self.inputFile= self.findChild(QtWidgets.QLineEdit, "inputFile")
@@ -96,18 +89,102 @@ class Ui(QtWidgets.QMainWindow):
         self.saveOutputFileButton.clicked.connect(self.saveOutputFilePressed)
         self.calculateButton= self.findChild(QtWidgets.QPushButton, "calculateButton")
         self.calculateButton.clicked.connect(self.calculatePressed)
-        #
         # INIT GUI
         self.init_gui()
 
-    # init gui
+    # key press handler
+    def keyPressEvent(self, event):
+        # load-table: delete items with delete-key
+        if event.key()==Qt.Key_Delete and self.tabWidget.currentIndex()==3:
+            for item in self.loadsTable.selectedItems():
+                item.setText('') 
+
+    # init gui - default settings
     def init_gui(self):
-        # fill bolt combo-box
+        # set radio-buttons
+        self.radioEsaPss.setChecked(True)
+        self.radioEcss.setEnabled(False) # not implemented yet
+        self.radioVdi.setEnabled(False) # not implemented yet
+        self.radioJointMin.setChecked(True)
+        self.radioJointMean.setEnabled(False) # not implemented yet
+        self.radioLockYes.setChecked(True)
+        # fill bolt combo-boxes
         for key in self.bolts.bolts:
             self.comboBolt.addItem(key)
-        # fill bolt-material combo-box
         for key in self.materials.materials:
             self.comboBoltMaterial.addItem(key)
+        # set number of shear planes and loading plane factor
+        self.numberOfShearPlanes.setValue(1)
+        self.loadingPlaneFactor.setText("0.5")
+        # fos tab
+        self.fosY.setText("1.1")
+        self.fosU.setText("1.25")
+        self.fosSlip.setText("1.1")
+        self.fosGap.setText("1.0")
+        # set delta-T
+        self.deltaT.setText("0")
+        # loads-table init
+        self.loadsTable.setColumnCount(4) # init load-table
+        self.loadsTable.insertRow(0)
+        self.loadsTable.setHorizontalHeaderLabels(\
+            ["Bolt-ID\n\nLoad-Case", "FN\n\n[N]", "FQ1\n\n[N]", "FQ2\n(optional)\n[N]"])
+        self.loadsTable.setItem(0,0,QtWidgets.QTableWidgetItem("test bolt"))
+        #
+        # clamped-parts-table
+        self.clampedPartsTable.setColumnCount(2) # init clamped-parts-table
+        self.clampedPartsTable.insertRow(0)
+        self.clampedPartsTable.setHorizontalHeaderLabels(\
+            ["Material\nor\nShim", "Thickness\n[mm]"])
+        header = self.clampedPartsTable.horizontalHeader()       
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)#ResizeToContents)
+        #header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+        combo = QtWidgets.QComboBox()
+        combo_box_options = ["bla1", "bla2"]
+        for t in combo_box_options:
+            combo.addItem(t)
+        self.clampedPartsTable.setCellWidget(0,0,combo)
+        self.clampedPartsTable.setVerticalHeaderItem(0, QtWidgets.QTableWidgetItem("CP(0)"))
+
+    # erase gui - reset / new
+    def erase_gui(self):
+        self.radioEsaPss.setChecked(True)
+        self.projectName.setText("new BAT project")
+        self.radioJointMin.setChecked(True)
+        self.comboBolt.setCurrentIndex(0)
+        self.comboBoltMaterial.setCurrentIndex(0)
+        # CoFs: [mu_head_max, mu_thread_max, mu_head_min, mu_thread_min]
+        self.cofBoltHeadMax.setText('')
+        self.cofThreadMax.setText('')
+        self.cofBoltHeadMin.setText('')
+        self.cofThreadMin.setText('')
+        # fill torques etc
+        self.tightTorque.setText('')
+        self.tightTorqueTol.setText('')
+        self.radioLockYes.setChecked(True)
+        self.prevailingTorque.setText('')
+        self.loadingPlaneFactor.setText('')
+        # clamped parts tab
+        self.cofClampedParts.setText('')
+        self.numberOfShearPlanes.setValue(1)
+        self.throughHoleDiameter.setText('')
+        self.substDiameter.setText('')
+        # fos tab
+        self.fosY.setText("1.1")
+        self.fosU.setText("1.25")
+        self.fosSlip.setText("1.1")
+        self.fosGap.setText("1.0")
+        # loads tab
+        self.deltaT.setText("0")
+        self.loadsTable.clearContents()
+        self.loadsTable.setRowCount(0)
+        self.loadsTable.insertRow(0)
+        self.loadsTable.setItem(0,0,QtWidgets.QTableWidgetItem("test bolt"))
+        # calculate tab
+        self.inputFile.setText('')
+        self.outputFile.setText('')
+        # finally set statusbar
+        self.statusbar.showMessage("New BAT project created")
 
     # use locking device radioButton logic for prevailing torque QLineEdit
     def lockRadioClicked(self):
@@ -128,12 +205,25 @@ class Ui(QtWidgets.QMainWindow):
 
     # add row button pressed
     def addRowPressed(self):
-        print("Add Row")
+        # current selection in table
+        selection = self.loadsTable.selectedIndexes()
+        if selection:
+            # insert row: selected cell (row+1)
+            self.loadsTable.insertRow(selection[0].row()+1)
 
     # delete row button pressed
     def deleteRowPressed(self):
-        print("Delete Row")
-
+        # get number of selections per row (in selection order)
+        row_sel = Counter([i.row() for i in self.loadsTable.selectedIndexes()])
+        # order row-number ascending
+        row_sel_asc = OrderedDict(sorted(row_sel.items()))
+        # key: row-number, value: selected-columns
+        # if selected-columns == 4 then complete row selected
+        row_offset = 0 # if >1 rows selected
+        for key, value in row_sel_asc.items():
+            if value==4:
+                self.loadsTable.removeRow(key-row_offset)
+                row_offset += 1
 
     # save input file button pressed
     def saveInputFilePressed(self):
@@ -149,7 +239,8 @@ class Ui(QtWidgets.QMainWindow):
 
     # MENU - new
     def menuNew(self):
-        print("new")
+        # reset gui - erase
+        self.erase_gui()
 
     # MENU - open input file
     def menuOpenInput(self):
@@ -215,20 +306,18 @@ class Ui(QtWidgets.QMainWindow):
             self.fosGap.setText(str(self.openedInputFile.fos_gap))
             # loads tab
             self.deltaT.setText(str(self.openedInputFile.delta_t))
+            self.loadsTable.setRowCount(0) # delete all rows
             for i, bi in enumerate(self.openedInputFile.bolt_loads):
-                if i > 0: # first line is inserted by default
-                    self.loadsTable.insertRow(i)
+                self.loadsTable.insertRow(i) # insert row
                 # fill bolt-loads into table
                 self.loadsTable.setItem(i,0,QtWidgets.QTableWidgetItem(bi[0]))
                 self.loadsTable.setItem(i,1,QtWidgets.QTableWidgetItem(str(bi[1])))
                 self.loadsTable.setItem(i,2,QtWidgets.QTableWidgetItem(str(bi[2])))
                 self.loadsTable.setItem(i,3,QtWidgets.QTableWidgetItem(str(bi[3])))
-
             # calculate tab
             self.inputFile.setText(openedFileName)
             outp_file = openedFileName.split('.')[0]+".out"
             self.outputFile.setText(outp_file)
-
             # finally set statusbar
             self.statusbar.showMessage("Input File Opened: "+openedFileName)
 
@@ -239,6 +328,11 @@ class Ui(QtWidgets.QMainWindow):
     # MENU - save as
     def menuSaveAs(self):
         print("save as")
+
+    # use data in clipboard and paste loads from excel
+    def pasteFromExcel(self):
+        text = QApplication.clipboard().text()
+        print(text+'\n')
 
 
 if __name__ == "__main__":
