d0d55085fbd5a350dc3cd97d43d4573c8979def1
misams <sams.m@gmx.net>
2021-04-04 10:31:54 +0200

    ThreadPullOut modified

--- BAT/src/ThreadPullOut.py
+++ BAT/src/ThreadPullOut.py
@@ -2,6 +2,7 @@ from src.functions.MaterialManager import MaterialManager
 from src.functions.BoltManager import BoltManager
 from pathlib import Path
 import math
+import matplotlib.pyplot as plt
 """
 Thread pull-out analysis acc. to VDI2230 §5.5.5
 """
@@ -12,6 +13,8 @@ class ThreadPullOut:
         self.bolts = bolts
         # calculate torque table
         self._calc_thread_pull_out()
+        # plot relative length of engagement
+        self._plot_rel_len_of_engagement()
 
     def _calc_thread_pull_out(self):
         # define bolt type and material
@@ -49,8 +52,43 @@ class ThreadPullOut:
             C_3 = 0.897 # if R_S >= 1.0
         # m_ges
         m_ges = self.materials.materials[bolt_mat].sig_u*b.As*b.p / (C_1 * C_3 * tau_BM \
-            *(b.p/2+(b.d-b.d2)*math.tan(30*math.pi/180))*math.pi*b.d) + b.p*2
+            *(b.p/2+(b.d-b.d2)*math.tan(30*math.pi/180))*math.pi*b.d) + b.p*0.8
         print("R_S = {0:.3f}".format(R_S))
         print("C_1 = {0:.3f}".format(C_1))
         print("C_3 = {0:.3f}".format(C_3))
         print("\nm_ges = {0:.2f} mm".format(m_ges))
+
+    def _plot_rel_len_of_engagement(self):
+        # set C1 and C3 equal 1
+        C_1 = 1.0
+        C_3 = 1.0
+        
+        bolt_mat = "10.9"
+
+        b = self.bolts.bolts["S_M4"] # used bolt
+        tau_plt_1 = []
+        e_min_1 = []
+        for tau_BM in range(100,500):
+            tau_plt_1.append(tau_BM)
+            e_min_1.append((self.materials.materials[bolt_mat].sig_u*b.As*b.p / (C_1 * C_3 * tau_BM \
+                *(b.p/2+(b.d-b.d2)*math.tan(30*math.pi/180))*math.pi*b.d) + b.p*0.8)/b.d)
+
+        b = self.bolts.bolts["S_M36"] # used bolt
+        tau_plt_2 = []
+        e_min_2 = []
+        for tau_BM in range(100,500):
+            tau_plt_2.append(tau_BM)
+            e_min_2.append((self.materials.materials[bolt_mat].sig_u*b.As*b.p / (C_1 * C_3 * tau_BM \
+                *(b.p/2+(b.d-b.d2)*math.tan(30*math.pi/180))*math.pi*b.d) + b.p*0.8)/b.d)
+
+
+        plt.plot(tau_plt_1, e_min_1, label="M4")
+        plt.plot(tau_plt_2, e_min_2, label="M36")
+        plt.xlabel("shear strength of internal thread material [MPa]")
+        plt.ylabel("relative length of engagement meff/d")
+        plt.grid(True)
+        plt.xlim([100,700])
+        plt.ylim([0, 4.5])
+        plt.legend(loc="upper right")
+        plt.show()
+
