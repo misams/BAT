66c22a416ba291e6476c1154ba07648025537486
misams <sams.m@gmx.net>
2021-09-06 20:21:14 +0200

    hashtag-bug for UNC/UNF bolts and washer corrected (fixes #11)

--- BAT/bat_main.py
+++ BAT/bat_main.py
@@ -14,9 +14,11 @@ import src.ThreadPullOut as thread_pull_out
 import src.functions.exceptions as ex
 import src.bat_qt_gui as bat_qt_gui
 
-__version__ = "0.8"
+__version__ = "0.8.1-ALPHA"
 """
 Change Log:
+v0.8.1 - xx.09.2021
+- corrected hastag-bug in UNC/UNF bolt and washer files
 v0.8 - 04.09.2021
 - slippage and gapping columns can be excluded in output (GUI only)
 - GUI does not crash anymore if saved empty (error info in command-line)
